647b4297b837c523d68be80675a26ce9c19010b5
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-30 14:04:20 -0500

    finished principal config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-black": "#2b2e30",
+        "primary-coffe": "#b77a37",
+        "yellow-dark": "#c78a35",
+        "yellow-mid": "#d59834",
+        "primary-yellow": "#e5a832",
+        "primary-white": "#e1e1e1",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
