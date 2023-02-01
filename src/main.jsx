df3f14549b64e6db7d96b97059255aef0acdbdb8
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-31 19:41:39 -0500

    store created

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/store/slices/username.slice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userName = createSlice({
+  name: "UserName",
+  initialState: "",
+  reducers: {
+    setUserNameGlobal: (state, action) => action.payload,
+  },
+});
+
+export const { setUserNameGlobal } = userName.actions;
+export default userName.reducer;
